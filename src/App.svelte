36f99654a3ab2cfd12ab8f1de7f2271baa1bd232
DeclanRoye <89390824+DeclanRoye@users.noreply.github.com>
2022-03-03 19:21:31 +0000

    Grid
    Grid

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,30 @@
 	import Butt from "./Button.svelte"
 	import T from "./Home.svelte"
 
+	
+	let grid = [7, 9];
+
 </script>
 
 
+<style>
+	div {
+		display: flex;
+		width: 100px;
+		justify-content: space-between;
+	}
+	
+	div span:first-child {
+		margin-right: 20px;
+	}
+	
+	input {
+		width: 100%
+	}
+</style>
 
-<Bar/>
+
+<Bar {grid}/>
 <Butt/>
 <T/>
 <main>
